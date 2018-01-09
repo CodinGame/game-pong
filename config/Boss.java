77afce667b8b9a4ae7ccaee62ffe98e8cfa57979
Nicolas Antoniazzi <nicolas@codingame.com>
2018-01-09 10:15:31 +0100

    Export configuration

---
+++ config/Boss.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Player {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int y = scanner.nextInt();
+            int oppY = scanner.nextInt();
+            int ballX = scanner.nextInt();
+            int ballY = scanner.nextInt();
+            int speedX = scanner.nextInt();
+            int speedY = scanner.nextInt();
+
+            int targetY = (int) Math.round(ballY + speedY * 1);
+
+            System.out.println(targetY);
+        }
+    }
+}
